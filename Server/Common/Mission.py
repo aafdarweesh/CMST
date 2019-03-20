1c05164a5af92ed27d8eb7272cb899ea0d738af3
aafdarweesh <aafdarweesh@gmail.com>
2019-03-21 00:34:36 +0200

    update files

--- Server/Common/Mission.py
+++ Server/Common/Mission.py
@@ -1,19 +1,25 @@
 import sys
 sys.path.append('../Common/')
 
+import json
 
 import DroneConfigurations
 import User
 
 class Mission :
-	def __init__(self, drone, user):
+	def __init__(self, drone, user, missionID):
 		self.MissionTime =  ""
 		self.DroneConfigurations = drone
-		self.userID = user.getID()
+		self.UserID = user.getID()
+		self.MissionID =  missionID
 
 	#set the mission start time
 	def setMissionStartTime(self, startTime):
 		self.MissionTime = startTime
+
+	#get mission id
+	def getMissionID(self):
+		return self.MissionID
 
 	#get the mission start time
 	def getMissionStartTime(self):
