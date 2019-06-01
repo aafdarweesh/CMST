1c61417f3a3c7bdcc4fa29421da440eda8125b59
aafdarweesh <aafdarweesh@gmail.com>
2019-06-01 18:39:22 +0300

    Update StartRecording.py
    Make the work recording of the videos

--- RaspberryPi/StartRecording.py
+++ RaspberryPi/StartRecording.py
@@ -6,18 +6,18 @@ import datetime
 duration = sys.argv[1]
 numberOfVideos = sys.argv[2]
 
-f=open("videoMetaData.txt", "a+")
-f.truncate(0)
 counter = 0
 camera = PiCamera()
 camera.resolution = (1920,1080)
 camera.framerate = 30
 camera.start_preview()
 while counter < int(numberOfVideos):
+        f=open("videoMetaData.txt", "a+")
         camera.start_recording('./videoBuffer/video' + str(counter) + '.h264')
         f.write("%d started %s %s\n" %(counter, "location",datetime.datetime.now())) #video number, status, location, datetime
         sleep(int(duration))
         camera.stop_recording()
         f.write("%d ended %s %s\n" %(counter, "location", datetime.datetime.now()))
         counter += 1
+        f.close()
 camera.stop_preview()
