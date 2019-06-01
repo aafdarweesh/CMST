34584add67e29ae08c6385b363e30f5061ac0c2c
aafdarweesh <aafdarweesh@gmail.com>
2019-06-01 11:00:24 +0300

    Update TestDifferentPorts.py
    Testing different ports numbers

--- Server/DetectionSystem/TestDifferentPorts.py
+++ Server/DetectionSystem/TestDifferentPorts.py
@@ -11,4 +11,6 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=8000)
+	app.run(host='0.0.0.0', debug=True, port=8083)
+
+#ports available : 80, 8000, 3000, 8082, 8083 (I guess only few that are reserved for something else)
