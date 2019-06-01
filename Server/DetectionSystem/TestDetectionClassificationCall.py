3e536be689aa007fccba8cdf761eb19f3c5a6713
aafdarweesh <aafdarweesh@gmail.com>
2019-06-01 11:55:36 +0300

    Update TestDetectionClassificationCall.py
    Implmenting the main function in the detection and classification communication program

--- Server/DetectionSystem/TestDetectionClassificationCall.py
+++ Server/DetectionSystem/TestDetectionClassificationCall.py
@@ -1,6 +1,65 @@
 
 import pickle
 import os
+
+
+#Common variables (Detection)
+location_of_detection_program = 'C:\\tensorflow1\\models\\research\\object_detection'
+name_of_detection_program = 'Object_detection_video.py'
+location_of_the_video = 'C:\\Users\\Administrator\\Desktop\\CMSTControllingSystem\\CMST\\Server\\DetectionSystem\\ReceivedData\\' #in our case the video's location
+
+
+#this function runs the detection system on the given video (in the directories specified above)
+def runDetectionSystem(file_name):
+
+	try:
+		os.system('activate tensorflow1 & cd ' + location_of_detection_program + ' & python ' + name_of_detection_program + ' ' + location_of_the_video + file_name)
+		return True
+	except:
+		print('Problem Running Video ' + str(file_name) + 'on the detection system')
+		return False
+		
+	
+#Common variables (Classification)
+location_of_classification_program = 'C:\\Users\\Administrator\\Desktop\\TurtleClassification'
+name_of_classification_program = 'TestCNN.py'
+location_of_the_image = 'C:\\New Project\\Detected\\Cropped\\'
+	
+#This function runs the classification system on the given image (in the directories specified above)
+def runClassificationSystem(file_name):
+	#write the address to the file 
+	F = open(location_of_classification_program + "\\TestImageAddress.txt","w") 
+	F.write(location_of_the_image + file_name)
+	F.close()
+
+	try:
+		os.system('activate MATLAB & cd ' + location_of_classification_program + ' & python ' + name_of_classification_program)
+		return True
+	except:
+		print('Problem Running image ' + str(file_name) + 'on the classification system')
+		return False
+
+
+
+#this function return list of files in directory (will be used to get the files detected and feed it to the classification system)
+def listOfFilesInDirectory(directory):
+	list_of_files = []
+	try:
+		list_of_files = os.listdir(directory)
+		return list_of_files
+	except:
+		print('No Such file or Directory Error for : ' + directory)
+		return list_of_files
+
+
+#this function returns list of names without the extension (will be used to get the frame id in that video, as we identify the name by the frame number)
+def listOfNamesWithoutExtension(list_of_data):
+	list_of_files_names = [] #list of files names without the extension
+	for x in list_of_files:
+		list_of_files_names.append(x.split('.')[0])	
+
+	return list_of_files_names
+
 '''
 
 def copyReceivedVideosToDetection():
