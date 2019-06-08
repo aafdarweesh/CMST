1f5fd2834e29f6caec9a2e1edf0e57bf7e687028
aafdarweesh <aafdarweesh@gmail.com>
2019-06-09 02:13:09 +0300

    Update test.py
    First Update on the mysql test

--- Server/DetectionSystem/test.py
+++ Server/DetectionSystem/test.py
@@ -15,7 +15,7 @@ import os
 #Tests
 ####################
 MAIN_DIRECTORY = 'C:\\CMSTData'
-missionID = 0
+missionID = 1
 video_number = videoID = 0
 
 location_of_the_video = MAIN_DIRECTORY + '\\' + str(missionID) + '\\ReceivedData\\' #in our case the video's location
@@ -34,7 +34,6 @@ if not os.path.exists(newpathUI):
 
 uiVideoLocation = 'C:\\ui_server\\htdocs\\Turtles\\CMSTData\\' + str(missionID) + '\\' + 'ReceivedData\\' + str(video_number) + '.mp4'
 os.rename(location_of_the_video + str(video_number) + '.mp4', uiVideoLocation)
-'''
 
 #Create Video folder for the videoID for UI (inside the DetectionFolder with (Detected, Cropped) subFolders
 def createVideoDirectoryUI(video_number):
@@ -66,4 +65,57 @@ os.rename('C:\\CMSTData\\' + str(missionID) +'\\DetectionFolder\\' + str(video_n
 #Move the cropped frame to the UI to be rendered
 uiFrameCroppedLocation = 'C:\\ui_server\\htdocs\\Turtles\\CMSTData\\' + str(missionID) +'\\DetectionFolder\\' + str(video_number) + '\\Cropped\\' + str(x) + '.jpg'
 os.rename('C:\\CMSTData\\' + str(missionID) +'\\DetectionFolder\\' + str(video_number) + '\\Cropped\\' + str(x) + '.jpg', uiFrameCroppedLocation)
+
+'''
+
+#Store the data in the database for that region
+#INSERT INTO `detection`.`detectedobject` (`sightingUrl`, `objectNumber`, `property1Value`, `objectName`, `accuracy`, `url`) VALUES ('resources/sightings/163937_web11.jpg', '1', 'Loggerhead', 'Sea Turtle', '50', '/resources/wallpaper.jpg');
+#Connect to the database
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="", database="detection")
+
+mycursor = mydb.cursor()
+
+
+#INSERT INTO `detection`.`sighting` (`sightingUrl`, `videoUrl`, `timeOfAppearance`, `numberOfObjects`) VALUES ('resources/sightings/123.png', 'resources/videos/video1.mp4', '7', '1');
+sqlMain1 = 'INSERT INTO detection.sighting (sightingUrl, videoUrl, timeOfAppearance, numberOfObjects) VALUES ('
+sqlMain1 += '\'' + './CMSTData/' + str(missionID) +'/DetectionFolder/' + str(video_number) + '/Detected/' + str(x) + '.jpg' + '\','
+sqlMain1 += '\'' + './CMSTData/' + str(missionID) +'/DetectionFolder/ReceivedData/' + str(video_number) + '.mp4\','
+sqlMain1 += '\'' + str(float(int(x)/30.0)) + '\', \'' + str(count_detected_objects) + '\')'
+
+#execute the sql code
+mycursor.execute(sqlMain1)
+mydb.commit()
+
+
+
+
+sqlMain = 'INSERT INTO detection.detectedobject (sightingUrl, objectNumber, property1Value, objectName, accuracy, url) VALUES ('
+sqlMain += '\'' + './CMSTData/' + str(missionID) +'/DetectionFolder/' + str(video_number) + '/Detected/' + str(x) + '.jpg' + '\''
+sql = ',\'' + str(i) + '\',\'' + str(classification_result[1]) + '\', \'Sea Turtle\', \'' + str(classification_result[0]) + '\', \''
+sql += './CMSTData/' + str(missionID) +'/DetectionFolder/' + str(video_number) + '/Cropped/' + str(x) + '-' + str(i) + '.jpg\')'
+
+#execute the sql code
+mycursor.execute(sqlMain + sql)
+mydb.commit()
+
+sql = ',\'1\',\'' + str(classification_result[1]) + '\', \'Sea Turtle\', \'' + str(classification_result[0]) + '\', \''
+sql += str('./CMSTData/' + str(missionID) +'/DetectionFolder/' + str(video_number) + '/Cropped/' + str(x) + '.jpg\')')
+
+#execute the sql code
+mycursor.execute(sqlMain + sql)
+mydb.commit()
+
+
+
+sql = 'INSERT INTO detection.video (videoUrl, missionID, latitude, longitude, startingTime) VALUES ('
+sql += '\''  + str('./CMSTData' + '/' + str(request.json['missionID']) + "/ReceivedData/" + videoName +'.mp4')
+sql += '\',\'' + str(request.json['missionID']) + '\', ' + str(lat) + ',' + str(lng) + ',' + str(int(videoName)*10) +')'
+
+mycursor.execute(sql)
+mydb.commit()
+
+
+
+				
+				
 
